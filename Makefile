e8f4bc82b56b9d879a56a19420ef15c31cb7b7e2
Adrian Siekierka <kontakt@asie.pl>
2024-12-12 18:16:44 +0100

    updater: add building .bin and .ws files

--- Makefile
+++ Makefile
@@ -1,6 +1,23 @@
-.PHONY: all clean program-fpga libnile libnile-ipl1 ipl0 ipl1 fpga
+include config.mk
 
-all: ipl0 ipl1 fpga
+DISTDIR  ?= out/dist
+MFGDIR   ?= out/manufacturing
+MANIFEST ?= manifest/full_update.txt
+UPDATEWS := $(DISTDIR)/fwupdate.ws
+FLASHBIN := $(MFGDIR)/spi.bin
+
+.PHONY: all clean help program-fpga program libnile libnile-ipl1 ipl0 ipl1 updater fpga
+
+all: ipl0 ipl1 fpga $(UPDATEWS) $(FLASHBIN)
+
+help:
+	@echo "nileswan build system"
+	@echo ""
+	@echo "all              Build all components"
+	@echo "                 User distributables are stored in $(DISTDIR)"
+	@echo "                 Manufacturing files are stored in $(MFGDIR)"
+	@echo "program-fpga     Build and program initial FPGA bitstream"
+	@echo "program          Build and program complete SPI flash contents"
 
 libnile:
 	cd software/libnile && make TARGET=wswan/medium
@@ -14,14 +31,30 @@ ipl0:
 ipl1: libnile-ipl1
 	cd software/ipl1 && make
 
+updater: libnile
+	cd software/updater && make
+
+$(FLASHBIN): fpga ipl1 $(MANIFEST) software/updater/manifest_to_bin.py
+	@mkdir -p $(@D)
+	python3 software/updater/manifest_to_bin.py $(MANIFEST) $@
+
+$(UPDATEWS): fpga ipl1 updater $(MANIFEST) software/updater/manifest_to_rom.py
+	@mkdir -p $(@D)
+	python3 software/updater/manifest_to_rom.py software/updater/updater_base.ws $(MANIFEST) $@
+
 fpga: ipl0
 	cd fpga && make
 
 program-fpga: fpga
 	cd fpga && make program
 
+program: $(FLASHBIN)
+	iceprog $<
+
 clean:
+	rm -rf out
 	cd software/libnile && rm -rf build
 	cd software/ipl0 && make clean
 	cd software/ipl1 && make clean
+	cd software/updater && make clean
 	cd fpga && make clean
---
+++ software/updater/manifest_to_bin.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+#
+# Copyright (c) 2024 Adrian Siekierka
+#
+# Nileswan Updater is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Nileswan Updater is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Nileswan Updater. If not, see <https://www.gnu.org/licenses/>.
+
+import argparse, crc, os, struct, subprocess, sys
+
+parser = argparse.ArgumentParser(prog='manifest_to_bin', description='Create SPI flash file from manifest')
+parser.add_argument('manifest', help='File describing the update contents')
+parser.add_argument('output_bin', help='Output BIN file (.bin)')
+args = parser.parse_args()
+
+write_at = {}
+max_size = 0
+
+updater_base_data = None
+with open(args.manifest, 'r') as rules:
+    for line in rules:
+        rule = line.strip()
+        if rule.startswith("#"):
+            continue
+        rule = rule.split(" ")
+        rule_name = rule[0]
+        rule_map = {}
+        for i in range(0, len(rule), 2):
+            rule_map[rule[i]] = rule[i+1]
+        if rule_name == 'FLASH' or rule_name == 'PACKED_FLASH':
+            data = None
+            with open(rule_map[rule_name], 'rb') as file:
+                data = file.read()
+            write_at[int(rule_map['AT'])] = data
+            max_size = max(max_size, int(rule_map['AT']) + len(data))
+
+with open(args.output_bin, 'wb') as fo:
+    fo.write(b'\xFF' * max_size)
+    for k, v in write_at.items():
+        fo.seek(k)
+        fo.write(v)
